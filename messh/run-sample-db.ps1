76372b0df31faa8b1c320c34e56442888fe7f462
Stanislav S. Yarmonov <stanislav@yarmonov.pro>
2022-10-05 19:41:47 +0300

    -Messh DB restart policy

--- messh/run-sample-db.ps1
+++ messh/run-sample-db.ps1
@@ -5,6 +5,7 @@ if (docker ps -a -q --filter "name=messh") {
 docker build -t messh .
 docker run -d `
     --name messh `
+    --restart unless-stopped `
     -e POSTGRES_PASSWORD=$(Read-Host "Enter a Password" -MaskInput) `
     -p 25432:5432 `
     -v e:/Data/messh:/var/lib/postgresql/data `
